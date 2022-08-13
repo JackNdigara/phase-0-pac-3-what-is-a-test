665df3f8ac0df3e67679124096bf9b3e4ebb8a31
peter jackson <peterndigara@gmail.com>
2022-08-13 20:40:26 +0300

    assgn done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+var name = "Joe"
+var height = 74
+var message = ` is ${height} inches tall`
 
-module.exports = {name, height, message};
+module.exports = {name, height, message}
--- test/index-test.js
+++ test/index-test.js
@@ -1,25 +1,26 @@
 const { name, height, message } = require("../index.js");
 
-/*
+var index = require("../index.js")
+
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
-      expect(name).toEqual('Susan')
+      expect(index.name).toEqual('Susan')
     })
   })
 
 
   describe('Height', () => {
-    it('is less than 40', () => {
-      expect(height).toBeLessThan(40)
+    it('is greater than 40', () => {
+      expect(index.height).toBeLessThan(40)
     })
   })
 
   describe('Message', () => {
     it('gives the name and height', () => {
-      expect(message).toInclude(name)
-      expect(message).toInclude(height)
+      expect(index.message).toInclude(index.name)
+      expect(index.message).toInclude(index.height)
     })
   })
+
 })
-*/
